ea21184fea3950d38af28510d6ebffba308556d2
krishna <msd07.captaincool@gmail.com>
2017-11-02 11:14:36 +0530

    kkkk

---
+++ src/test/java/com/iqtc/junit/LoginTest.java
@@ -0,0 +1,38 @@
+package com.iqtc.junit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.iqtc.dao.LoginDao;
+import com.iqtc.dao.UserDaoImpl;
+
+import junit.framework.TestCase;
+
+public class LoginTest {
+	
+	@Before
+	public void beforeTest()
+	{
+		System.out.println("Before Test Run...");
+		
+	}
+	
+	@Test
+	public void LoginTest()
+	{
+		LoginDao user=new UserDaoImpl();
+		assertTrue(user.ValidateUser("admin","12345"));
+		
+	}
+	
+	
+	@After
+	public void afterTest()
+	{
+		System.out.println("After Test Run .......");
+	}
+
+}
--- target/Spring-V1/index.jsp
+++ target/Spring-V1/index.jsp
@@ -1,5 +1,6 @@
 <html>
 <body>
+<h2>Bitbucket Code</h2>
 <form action="add">
 <input type="text" name="uname">
 <input type="text" name="password">
